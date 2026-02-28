0965b7dec0c1791b71e7a0947dc58361dd049364
Hamza Erfan <wmis222@live.rhul.ac.uk>
2026-02-28 18:11:05 +0000

    Test user created

--- src/main/java/com/example/dashboard/DashboardApplication.java
+++ src/main/java/com/example/dashboard/DashboardApplication.java
@@ -1,7 +1,15 @@
 package com.example.dashboard;
 
+import java.time.LocalDateTime;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.dashboard.entity.User;
+import com.example.dashboard.repository.UserRepository;
 
 @SpringBootApplication
 public class DashboardApplication {
@@ -9,5 +17,22 @@ public class DashboardApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DashboardApplication.class, args);
 	}
+	@Bean
+    CommandLineRunner init(UserRepository userRepository,
+                           PasswordEncoder encoder) {
+        return args -> {
+            if (userRepository.findByEmail("erfan@example.com").isEmpty()) {
 
+                User user = new User();
+                user.setName("Erfan");
+                user.setEmail("erfan@example.com");
+                user.setPasswordHash(encoder.encode("password"));
+                user.setRole("USER");
+                user.setCreatedAt(LocalDateTime.now());
+
+                userRepository.save(user);
+                System.out.println("Test user created.");
+            }
+        };
+    }
 }
